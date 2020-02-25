b6501f53ca165cb93b1af5ce9e91674f05cdc3ce
afdulfauzan <afdulfauzan.id@gmail.com>
2020-02-25 16:43:09 +0700

    fixed stickers.py #2

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -86,8 +86,8 @@ async def kang(args):
                 # pack
                 emoji = splat[1]
 
-        packname = f"@{user.username} - {pack}"
-        packnick = f"@{user.username} - {pack}"
+        packname = f"{user.username}{pack}"
+        packnick = f"@{user.username} pack {pack}"
         cmd = '/newpack'
         file = io.BytesIO()
 
@@ -114,8 +114,8 @@ async def kang(args):
                 x = await conv.get_response()
                 while "120" in x.text:
                     pack += 1
-                    packname = f"@{user.username} - {pack}"
-                    packnick = f"@{user.username} - {pack}"
+                    packname = f"{user.username}{pack}"
+                    packnick = f"@{user.username} pack {pack}"
                     await args.edit("`Switching to Pack " + str(pack) +
                                     " due to insufficient space`")
                     await conv.send_message(packname)
